f825957d82730caee2ee8f28102e4603d7bd5903
Kallaf <kalaf1997@yahoo.com>
2020-05-18 23:56:18 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# Daily Deals
+
+The Daily Deals app displays a list of deals and discounts on various products. We'll have a list of publicly available deals that anyone can see and a list of private deals available only to registered members. The private deals are exclusive to registered members, and should hopefully be better.
+
+## How to run
+- for server 
+```
+cd server
+npm install
+node server/app.js
+```
+- for client
+```
+cd client
+npm install
+ng serve
+```
---
+++ server/app.js
@@ -0,0 +1,54 @@
+'use strict';
+// Load dependencies
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
+
+// Public route
+app.get('/api/deals/public', (req, res) => {
+    let deals = [
+        {
+            id: 1234,
+            name: 'Name of Product',
+            description: 'Description of Product',
+            originalPrice: 19.99, // Original price of product
+            salePrice: 9.99 // Sale price of product
+        },{
+            id: 1235,
+            name: 'Name of Product',
+            description: 'Description of Product',
+            originalPrice: 19.99, // Original price of product
+            salePrice: 9.99 // Sale price of product
+        }
+    ];
+    res.json(deals);
+})
+
+// Private route
+app.get('/api/deals/private', (req, res) => {
+    let deals = [
+        // Array of Private Deals here
+        {
+            id: 1236,
+            name: 'Name of Product',
+            description: 'Description of Product',
+            originalPrice: 19.99, // Original price of product
+            salePrice: 9.99 // Sale price of product
+        },{
+            id: 1237,
+            name: 'Name of Product',
+            description: 'Description of Product',
+            originalPrice: 19.99, // Original price of product
+            salePrice: 9.99 // Sale price of product
+        }
+    ];
+    res.json(deals);
+})
+
+app.listen(3001);
+console.log('Serving deals on localhost:3001');
